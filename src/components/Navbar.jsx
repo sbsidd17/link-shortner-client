d87cdc89da2d18c7a1b2afb5515990bdcc617558
Shuaib Siddiqui <122801841+sbsidd17@users.noreply.github.com>
2023-08-18 15:00:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -126,6 +126,20 @@ export default function Navbar({ isLoggedIn, setIsLoggedIn }) {
                       {isLoggedIn && (
                         <Menu.Item>
                           {({ active }) => (
+                            <p
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              {name}
+                            </p>
+                          )}
+                        </Menu.Item>
+                      )}
+                      {isLoggedIn && (
+                        <Menu.Item>
+                          {({ active }) => (
                             <Link
                               to="/profile"
                               className={classNames(
